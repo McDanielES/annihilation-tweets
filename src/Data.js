d5a6de2da634818c3ffa19981ca21ddabc26297d
Eric McDaniel <McDanielES@gmail.com>
2021-01-20 03:14:40 -0600

    Read all tweets from local fs JSON file, build stream in component

--- src/Data.js
+++ src/Data.js
@@ -20,11 +20,37 @@ const tweetData = data.map( (data)  => {
       app: data.app,
       retweets: data.retweets,
       quotedTweets: data.quotedTweets,
-      likes: data.likes
+      likes: data.likes,
+      key: data.key
     }
     return (
       <FakeTweet config={tweetConfiguration}/>
     );
 });
+function buildConfigObj() {
+  let assembledTweets = [];
+  tweetData.forEach( e => {
+    let part  = {};
+    part.user = {};
 
-export default tweetData;
+    part.user.nickname = e.props.config.user.nickname;
+    part.user.name = e.props.config.user.name;
+    part.user.avatar = e.props.config.user.avatar;
+    part.user.verified = e.props.config.user.verified;
+    part.user.locked = e.props.config.user.locked;
+    part.display = e.props.config.display;
+    part.text = e.props.config.text;
+    part.image = e.props.config.image;
+    part.date = e.props.config.date;
+    part.app = e.props.config.app;
+    part.retweets = e.props.config.retweets;
+    part.quotedTweets = e.props.config.quotedTweets;
+    part.likes = e.props.config.likes;
+    part.key = e.props.config.key;
+    
+    assembledTweets.push(part);
+  })
+  return assembledTweets;
+}
+
+export default buildConfigObj;
--- src/Stream.js
+++ src/Stream.js
@@ -1,56 +1,18 @@
 import React, { Component } from 'react';
 import Tweet from './Tweet';
-import Data from './Data';
+import buildConfigObj from './Data';
 
 class Stream extends Component {
   render() {
-    const config = {
-        user: {
-          nickname: "biologist",
-          name: "biologist",
-          avatar: "favicon.ico",
-          verified: true,
-          locked: false
-        },
-        display: "default",
-        text: "Look at this doe",
-        image: "https://www.ericmcd.com/imgs/kevin.jpg",
-        date: "3:32 PM · Feb 14, 1997",
-        app: "Twitter for iPhone",
-        retweets: 32674,
-        quotedTweets: 1597,
-        likes: 12700
-      };
-      const config2 = {
-        user: {
-          nickname: "biologist",
-          name: "biologist",
-          avatar: "favicon.ico",
-          verified: true,
-          locked: false
-        },
-        display: "default",
-        text: "Oh c'mon, man!",
-        image: "",
-        date: "3:32 PM · Feb 14, 1997",
-        app: "Twitter for iPhone",
-        retweets: 32674,
-        quotedTweets: 1597,
-        likes: 12700
-      };
-      
-      const data = Data;
-      console.log(data[0]);
+    const data = buildConfigObj();
+    let tweetStream = [];
+    data.forEach(element => {
+      tweetStream.push(<Tweet key={element.key} details={element} />)
+    });
 
-
-
-      
-
-      
     return(
       <div>
-        <Tweet details={config}/>
-        <Tweet details={config2} />
+        { tweetStream }
       </div>
     );
   }
